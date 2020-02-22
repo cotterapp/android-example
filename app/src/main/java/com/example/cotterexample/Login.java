4e4f0f66e3657f134f57b0e57e769018a353c904
Putri Karunia <putri@go-vest.com>
2020-02-21 18:49:55 -0800

    Add email and phone input

--- app/src/main/java/com/example/cotterexample/Login.java
+++ app/src/main/java/com/example/cotterexample/Login.java
@@ -16,11 +16,18 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
-    public void login(View view) {
+    public void loginEmail(View view) {
         Cotter.newIdentity(this).login("EMAIL", this, Dashboard.class);
     }
-    public void loginWithInput(View view) {
+    public void loginEmailWithInput(View view) {
         EditText input = findViewById(R.id.input);
         Cotter.newIdentity(this).loginWithInput("EMAIL", input.getText().toString(), this, Dashboard.class);
+    }
+    public void loginPhone(View view) {
+        Cotter.newIdentity(this).login("PHONE", this, Dashboard.class);
+    }
+    public void loginPhoneWithInput(View view) {
+        EditText input = findViewById(R.id.inputPhone);
+        Cotter.newIdentity(this).loginWithInput("PHONE", input.getText().toString(), this, Dashboard.class);
     }
 }
